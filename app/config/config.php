574ae1e5b1111964c9dd71df7279cbe90f643311
Anwarsyah <62179765+nwarsyh@users.noreply.github.com>
2026-06-19 21:24:09 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 date_default_timezone_set('Asia/Jakarta');
-define('BASEURL', 'http://localhost/githubanwar/simpada'); //hapus saat digithubgithubanwar
+define('BASEURL', 'http://localhost/simpada');
 define('BASE_PATH', dirname(__DIR__, 2));
 define('ASSET_PATH', BASE_PATH . '/assets');
 define('ASSET_URL', BASEURL . '/assets');
